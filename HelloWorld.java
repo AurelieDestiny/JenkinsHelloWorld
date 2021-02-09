88aecb6f6243435dfed149761b82f9a39efc1ada
AurelieDestiny <78560576+AurelieDestiny@users.noreply.github.com>
2021-02-08 19:02:55 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String args[])
-    {System.out.println("Hello! I'm and see my first script in Jenkins");
+    {System.out.println("Hello! In fact, this is something awesome I am leaning today. I hope I get to understand everything");
     }
 }
